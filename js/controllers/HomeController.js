91a61618fb5ac7773f168c836829ad61b4b008f3
beigid <beigid@seattleu.edu>
2018-10-03 18:36:27 -0700

    Added unread and read buttons

--- js/controllers/HomeController.js
+++ js/controllers/HomeController.js
@@ -1,5 +1,9 @@
 app.controller('HomeController', ['$scope', 'emails', function($scope, emails) {
   emails.success(function(data) {
     $scope.emails = data;
+    $scope.unreadCheck = function() {
+      //return $scope.emails[index].unread == false;
+      document.querySelector(".btn").style.display = "none";
+    }; 
   });
 }]);
