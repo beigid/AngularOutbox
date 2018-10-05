f940d419485403672803bf91624d9e81a5b85349
beigid <beigid@seattleu.edu>
2018-10-04 20:16:11 -0700

    Fixed the boolean for unread

--- js/controllers/HomeController.js
+++ js/controllers/HomeController.js
@@ -1,9 +1,10 @@
 app.controller('HomeController', ['$scope', 'emails', function($scope, emails) {
   emails.success(function(data) {
     $scope.emails = data;
-    $scope.unreadCheck = function() {
-      $scope.emails.unread == false;
-      console.log("test for button switch")
-    }; 
   });
+$scope.unreadCheck = function(index) {
+	$scope.emails[index].unread = false;
+
+};
+  
 }]);
